9ee5e6c28a7c70e5a7c032ad23524b4961aaf2b2
Ingrid <134698608+ingridcrruz@users.noreply.github.com>
2025-09-26 11:40:35 -0300

    at

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+window.addEventListener('load',() => {
+  alert("Bem-vindo!");
+});
+
 const imagens = document.querySelectorAll('.fotos img');
 imagens.forEach(img => {
   img.addEventListener('click', () => {
     img.style.border = '3px solid #222';
   });
-});
-
-window.addEventListener('load',() => {
-  alert("Bem-vindo!");
 });
